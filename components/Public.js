8b5d1a663b6b6283061ab849c2979b6fe662e6f7
Sebastian009w <ia3266595@gmail.com>
2023-04-18 22:12:55 -0500

    problemas con supabase al utilizar storage

--- components/Public.js
+++ components/Public.js
@@ -1,7 +1,48 @@
+import { useState } from "react";
+import { supabase } from "@/utils/db.js";
+
 function Public() {
+  const [alt, setAlt] = useState("");
+  const [url, setUrl] = useState("");
+  const [err, setErr] = useState("");
+
+  const handleSubmimt = async (e) => {
+    e.preventDefault();
+    console.log("event");
+
+    if (!url) {
+      setErr("PORFAVOR SELECCIONA UNA IMAGEN");
+      return;
+    }
+    const file = url.name.split(".").pop();
+    const fileName = `${Math.random().toString(36).substring(7)}.${file}`;
+    const filePath = `${fileName}`;
+    const response = supabase.storage.from("public").getPublicUrl();
+
+    console.log(response);
+  };
   return (
-    <div>Public</div>
-  )
+    <div>
+      <span>{err}</span>
+      <form onSubmit={handleSubmimt} encType="multipart/form-data">
+        <label>
+          Alt:
+          <input
+            type="text"
+            placeholder="Ingresa un Titutlo"
+            onChange={(e) => setAlt(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Image:
+          <input type="file" onChange={(e) => setUrl(e.target.files[0])} />
+        </label>
+        <br />
+        <button type="submit">PUBLICAR</button>
+      </form>
+    </div>
+  );
 }
 
-export default Public
+export default Public;
