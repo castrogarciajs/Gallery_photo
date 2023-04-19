1c0146d3ec1d54233ecf1bc972e4f4cdee5e3b14
Sebastian009w <ia3266595@gmail.com>
2023-04-18 21:30:54 -0500

    componet public

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,4 @@
-import Header from "@/components/Layout.js";
+import Header from "@/components/Header.js";
 
 function Layout({ children }) {
   return (
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
+import { supabase } from "@/utils/db.js";
+
 function Home() {
+  
   return <h1>HOME</h1>;
 }
+
 
 export default Home;
